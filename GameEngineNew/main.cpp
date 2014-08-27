df3dc731dc9158ca5a31b6186f99b413dc49da98
Marcus Gabilheri <marcusandreog@gmail.com>
2014-08-26 19:55:28 -0500

    Initial Commit

---
+++ GameEngineNew/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GameEngineNew
+//
+//  Created by Marcus Gabilheri on 8/26/14.
+//  Copyright (c) 2014 Marcus Gabilheri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
