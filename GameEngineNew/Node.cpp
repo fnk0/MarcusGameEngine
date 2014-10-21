bc8855e76a03be4192b7eb9e418b268bf188ce60
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-20 22:22:41 -0500

    Fixed some stuff

--- GameEngineNew/Node.cpp
+++ GameEngineNew/Node.cpp
@@ -14,14 +14,23 @@ void Node::refreshTransforms() {
     Transform childrenTransform;
 
     for(int i = 0; i < nodes.size(); i++) {
+        nodes[i]->refreshTransforms();
         childrenTransform.transform *= nodes[i]->T.transform;
     }
-    
+
     if(parent != NULL) {
-        T.transform *= parent->T.transform * childrenTransform.transform;
+       T.transform *= parent->T.transform * childrenTransform.transform;
     } else {
         T.transform *= childrenTransform.transform;
     }
-    
-    
+
+
+}
+
+void Node::draw(glm::mat4x4 &mat, glm::mat4x4 &matInverse, SceneCamera &camera)
+{
+    T.refreshTransform();
+    meshInstance->material.bindMaterial(T, camera);
+    if (meshInstance != NULL) meshInstance->draw(camera);
+    else printf("Error! Null Mesh.");
 }
--- GameEngineNew/Scene.h
+++ GameEngineNew/Scene.h
@@ -84,6 +84,7 @@ private:
     map<std::string, RGBAImage*> textures;
     vector<MeshInstance*> meshInstances;
     vector<SceneCamera*> cameras;
+    ISoundEngine* soundEngine;
 
 public:
     vector<Light> lights;
@@ -157,7 +158,16 @@ public:
     void setGWindow(GLFWwindow *gWindow) {
         Scene::gWindow = gWindow;
     }
-    
+
+
+    ISoundEngine *getSoundEngine() const {
+        return soundEngine;
+    }
+
+    void setSoundEngine(ISoundEngine *soundEngine) {
+        Scene::soundEngine = soundEngine;
+    }
+
     void switchCamera(int camNum);
 
     void updateLights(void) {
